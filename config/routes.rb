6a4a98349385d79e72e054f0e18b3d3037cd8d60
TheGerrex <gerrex95@gmail.com>
2022-03-30 18:59:05 -0600

    added signup page form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get "signup", to: "users#new"
+  post "users", to: "users#create"
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
