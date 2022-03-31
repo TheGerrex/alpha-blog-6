556c700b4608c3ecfbb55215d93952277287168d
TheGerrex <gerrex95@gmail.com>
2022-03-30 23:59:14 -0600

    Add edit template and update action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except: [:new]
   root 'pages#home'
   get 'about', to: 'pages#about'
   get "signup", to: "users#new"
-  get "create", to: "users#create"
-  resources :users, except: [:new]
+  # get "create", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
