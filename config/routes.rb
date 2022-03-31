31f60409e70402487150c8695831a55375721372
TheGerrex <gerrex95@gmail.com>
2022-03-30 19:13:23 -0600

    added functionality of users create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get "signup", to: "users#new"
-  post "users", to: "users#create"
+  get "create", to: "users#create"
   resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
