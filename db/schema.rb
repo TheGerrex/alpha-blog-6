85f6018c02f8497f8b71370c1543605369730379
TheGerrex <gerrex95@gmail.com>
2022-03-30 18:37:53 -0600

    added BCrypt gem and user has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_235643) do
+ActiveRecord::Schema.define(version: 2022_03_31_003156) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_30_235643) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
